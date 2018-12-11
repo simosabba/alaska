f68d6d3007f52f4af1d2b248458f65e86f715f01
Simone Sabba <Simone.Sabba@alpitourworld.it>
2018-12-11 18:59:01 +0100

    god moved

--- src/demo/Alaska.Demo.WebApp/Startup.cs
+++ src/demo/Alaska.Demo.WebApp/Startup.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Alaska.Demo.WebApp.Infrastructure.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -32,13 +31,7 @@ namespace Alaska.Demo.WebApp
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-            services.AddEntityContext<AlaskaEntityContext>(opt => 
-            {
-                opt.Collections.ConnectionString = Configuration.GetConnectionString("Alaska");
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
